0665aa48686641e037bd8cc497ead7307e418c0c
yuva1918 <142742467+yuva1918@users.noreply.github.com>
2023-08-20 18:19:16 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,5 +6,9 @@ streamlit.header ('Breakfast Menu')
 
 streamlit.write ('Omega 3 & Blueberry Oat meal')
 
+streamlit.write ('Kale , Spinach & Rocket Smoothie')
+
+streamlit.write ('Hard-Boiled Free Range Eggs')
+
 
 
